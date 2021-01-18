21254e09d499047bc559dffeb66cf58e4ed8b5bd
Praveen Oruganti <praveenorugantitech@gmail.com>
2021-01-18 18:19:22 +0530

    All About React JS

---
+++ README.md
@@ -0,0 +1,10 @@
+# Cocktails App developed using React JS with Hooks
+
+![screenshot of the app](https://raw.githubusercontent.com/praveenorugantitech/praveenorugantitech-reactjs/master/0_Projects/praveenorugantitech-cocktails/src/images/screenshot.PNG "Cocktails App")
+
+
+[Live Demo](https://praveenorugantitech-cocktails.firebaseapp.com/)
+
+
+
+
---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+      An open, crowd-sourced database of drinks and cocktails from around the world.
+      We will display only 100 different variaties of cocktails for free license.
+      </p>
+    </section>
+  );
+}
